82d41b0a346747c534e18f636736a108fe1d4486
yahyabenjdy <100426617+yahyabenjdy@users.noreply.github.com>
2023-03-06 17:25:26 +0100

    clear-list

--- src/App.js
+++ src/App.js
@@ -16,20 +16,29 @@ function App() {
     e.preventDefault();
     if (!name) {
       // display alert
-      setAlert({ show: true, mssg: "please enter valuue", type: "danger" });
+      showAlert(true, "danger", "please enter value");
     } else if (name && isEditing) {
       // deal with edit
     } else {
       // show alert
+      showAlert(true, "success", "item added to the list");
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
       setName("");
     }
   };
+
+  const showAlert = (show = false, type = "", msg = "") => {
+    setAlert({ show, type, msg });
+  };
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
+  };
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert {...alert} />}
+        {alert.show && <Alert {...alert} removeAlert={showAlert} />}
         <h3>Grocery bud</h3>
         <div className="form-control">
           <input
@@ -47,7 +56,9 @@ function App() {
       {list.length > 0 && (
         <div className="grocery-container">
           <List items={list} />
-          <button className="clear-btn">Clear items</button>
+          <button className="clear-btn" onClick={clearList}>
+            Clear items
+          </button>
         </div>
       )}
     </section>
